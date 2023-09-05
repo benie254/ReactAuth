cbb04865820a1e02693842cfd5eecca6d7a34e10
benie254 <davinci.monalissa@gmail.com>
2023-09-05 14:25:21 +0300

    update auth service to handle jwt token

--- auth/src/auth/AuthService.ts
+++ auth/src/auth/AuthService.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const API_URL = "https://ben-in-ke-backend-nvp1.vercel.app/api/";
+const SECRET_KEY = '';
+
 
 export const AuthService = {
     signup: async (username: string, email: string, first_name: string, last_name: string, password: string): Promise<void> => {
@@ -12,16 +15,25 @@ export const AuthService = {
         }
     },
 
-    login: async (email: string, password: string): Promise<string | null> => {
+    login: async (email: string, password: string): Promise<string | JwtPayload | null> => {
         try {
             const response = await axios.post(`${API_URL}/user/auth/login`, {email, password});
-            return response.data.token;
+            const { token } = response.data;
+
+            // verify & decode the token
+            const decodedToken = jwt.verify(token, SECRET_KEY);
+
+            // store the token in localStorage
+            localStorage.setItem('user', token);
+
+            return decodedToken;
         } catch (error) {
             throw error;
         }
     },
 
     logout: () => {
-        // logout logic here
+        // clear token from localStorage
+        localStorage.removeItem('user');
     }
 }
